9690925605d0b3f65d7cc70525c4c2e5d882b0d6
Daniel Skýpala <daniel@honza.info>
2021-10-14 17:42:28 +0200

    Added variable evaluation

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
+import re
 import sys
 import os
 import json
+from copy import copy, deepcopy
 
 
 def load_path(*paths):
@@ -17,24 +19,72 @@ def read_file(path):
     return content
 
 
-def add_variable(key, value):
-    global variables
+def add_variable(key, value, variables):
     if key in variables:
-        raise Exception(f"Duplicate key {key}")
+        raise Exception(f"Duplicate key {key} in variables.")
+    variables[key] = value
+
+
+def load_variable(name, variables, loaded_variables):
+    if name not in variables:
+        raise Exception(f"Unknown name of variable {name}.")
+
+    if loaded_variables[name] is False:
+        variables[name] = load_content(variables[name])
+        loaded_variables[name] = True
+    return variables[name]
+
+
+def load_content(value, variables, loaded_variables):
+    i = 0
+    loading_variable = 0
+    loading = [""]
+
+    while i < len(value):
+        add = ""
+        if i > 0 and value[i-1] == "\\":
+            loading[-1] += value[i]
+
+        elif value[i] == "{":
+            loading_variable += 1
+            loading.append("")
+
+        elif value[i] == "}":
+            loading_variable -= 1
+            if loading_variable < 0:
+                raise Exception("'}' before '{' in " + value + ".")
+            loading[loading_variable] += load_variable(loading.pop(), variables, loaded_variables)
+
+        else:
+            loading[-1] += value[i]
+        i += 1
+
+    if loading_variable > 0:
+        raise Exception("No matching parenthesis for '{' in " + value + ".")
+    return loading[0]
+
+
+def load_variables(config_dir, config):
+    variables = deepcopy(config["variables"])
+    for variableName, variableFile in config["variableFiles"].items():
+        path = load_path(config_dir, "fileVariables", variableFile)
+        add_variable(variableName, read_file(path), variables)
+
+    # TODO add script variables
+    print(variables)
+
+    loaded_variables = {key: key not in config["variables"] for key in variables}
+
+    for variable in variables:
+        load_variable(variable, variables, loaded_variables)
 
 
 if len(sys.argv) < 3:
-    raise Exception("Not enough arguments")
+    raise Exception("Not enough arguments.")
 
-config_file = load_path(sys.argv[1])
+config_dir = load_path(sys.argv[1])
 path = load_path(sys.argv[2])
 
-config = json.loads(read_file(config_file))
+config = json.loads(read_file(os.path.join(config_dir, "config.json")))
 
-variables = config["variables"]
-for variableName, variableFile in config["variableFiles"].items():
-    path = load_path(os.path.dirname(config_file), variableFile)
-    add_variable(variableName, read_file(path))
-
-for a, b in variables.items():
-    print(a, b)
+load_variables(config_dir, config)
