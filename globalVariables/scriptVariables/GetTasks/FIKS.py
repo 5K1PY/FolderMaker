6f5cb223aca0c07c17dca1e7a365644ecce50069
Daniel Skýpala <daniel@honza.info>
2022-02-25 19:23:43 +0100

    Fiks config: According files to task type

--- globalVariables/scriptVariables/GetTasks/FIKS.py
+++ globalVariables/scriptVariables/GetTasks/FIKS.py
@@ -8,37 +8,73 @@ def get_el(tree, xpath):
 
 
 @cache
-def load(url):
+def load_raw(url):
     response = requests.get(url)
     if response.status_code != 200:
         raise ConnectionError(f"Something faied, request to URL '{url}' returned {response.status_code}")
     soup = BeautifulSoup(response.content, "html.parser")
-    names = []
+    divs = []
     for div in soup.find_all("div", class_="panel panel-default"):
-        names.append((
+        divs.append(div)
+    return divs
+
+
+def load(url):
+    tasks = []
+    for task_num, div in enumerate(load_raw(url)):
+        if "Odpověz Sfinze!" in div.text:
+            flag = "practical"
+        elif "Rozmysli, popiš a naprogramuj!" in div.text:
+            flag = "mixed"
+        elif "Zamysli se!" in div.text:
+            flag = "theoretical"
+
+        tasks.append([
+            task_num+1,
             div.find("a").text.strip(),
-            "kola lze řešit i v" in div.find("div", class_="panel-body").text.strip()
-        ))
-    return names
+            "kola lze řešit i v" in div.find("div", class_="panel-body").text.strip(),
+            flag
+        ])
+    return tasks
 
 
 def filter_new_tasks(tasks):
-    return list(filter(lambda x: not x[1], tasks))
+    tasks = list(filter(lambda x: not x[2], tasks))
+    for i in range(len(tasks)):
+        tasks[i][0] = i+1
+    return tasks
 
 
-def ordered_tasks(tasks):
-    return list(map(lambda x: (x[0]+1, x[1][0]), enumerate(tasks)))
+def beautify(tasks):
+    return list(map(lambda x: f"{x[0]} {x[1]}", tasks))
 
 
+def categorize(tasks, category):
+    return list(filter(lambda x: x[3] == category, tasks))
+
+
+# task returning functions - use these
 def load_all_tasks(url):
-    return list(map(lambda x: f"{x[0]} {x[1]}", ordered_tasks(load(url))))
+    return beautify(load(url))
 
 
 def load_new_tasks(url):
-    return list(map(lambda x: f"{x[0]} {x[1]}", ordered_tasks(filter_new_tasks(load(url)))))
+    return beautify(filter_new_tasks(load(url)))
+
+
+def load_tasks_categorized(url, category):
+    return beautify(categorize(load(url), category))
+
+
+def load_new_tasks_categorized(url, category):
+    return beautify(categorize(filter_new_tasks(load(url)), category))
 
 
 if __name__ == "__main__":
     url = "https://fiks.fit.cvut.cz/ulohy/"
     print(load_all_tasks(url))
     print(load_new_tasks(url))
+    for category in ("practical", "mixed", "theoretical"):
+        print(category)
+        print("\t", load_tasks_categorized(url, category))
+        print("\t", load_new_tasks_categorized(url, category))
